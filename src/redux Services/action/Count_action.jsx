dbfea165d953ae9ca5646ecc1fedea9887023700
AVINASH SINGH <83054042+Avinashvns@users.noreply.github.com>
2023-12-23 08:43:10 +0530

    add action (count)

--- src/redux Services/action/Count_action.jsx
+++ src/redux Services/action/Count_action.jsx
@@ -1,2 +1,15 @@
+export const Increment = 'INCREMENT';
+export const Decrement = 'DECREMENT';
 
+export const incrementCounter = () => {
+    return {
+        type: Increment
+    }
+}
+
+export const decrementCounter = () => {
+    return {
+        type: Decrement
+    }
+}
 
