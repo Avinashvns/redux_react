2163e881d01715edf5591656f83f61b5c3b57160
AVINASH SINGH <83054042+Avinashvns@users.noreply.github.com>
2023-12-23 09:25:34 +0530

    call count page in App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import MyCounter from './count';
 
 function App() {
   return (
     <div className="App">
       <h1 style={{ textAlign: 'center' }}>Welcome</h1>
+      <br />
+      <MyCounter />
     </div>
   );
 }
