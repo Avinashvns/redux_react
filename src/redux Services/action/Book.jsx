ce59a99196d638491958ec64f44dda3b92675fa8
AVINASH SINGH <83054042+Avinashvns@users.noreply.github.com>
2023-12-23 10:40:09 +0530

    Add book action

---
+++ src/redux Services/action/Book.jsx
@@ -0,0 +1,15 @@
+
+export const mytitle = 'TITLE';
+export const myauthor = 'AUTHOR';
+
+export const bookTitle = () => {
+    return {
+        type: mytitle
+    }
+}
+
+export const bookAuthor = () => {
+    return {
+        author: myauthor
+    }
+}
