74dbd721abb51d4d7e2cc02873083a2045d47c1c
AVINASH SINGH <83054042+Avinashvns@users.noreply.github.com>
2023-12-23 08:53:13 +0530

    add provider createStore & reducer

--- src/index.js
+++ src/index.js
@@ -4,11 +4,24 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import counterReducer from './redux Services/reducer/Count_reducer';
+
+const store = createStore(counterReducer);
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+ReactDOM.render(
+  <Provider store={store} >
     <App />
-  </React.StrictMode>
+  </Provider>,
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
