8fd0c16f67adab196470c7f80e6a88968e700fd8
AVINASH SINGH <83054042+Avinashvns@users.noreply.github.com>
2023-12-23 08:47:26 +0530

    add reducer (count)

---
+++ src/redux Services/reducer/Count_reducer.jsx
@@ -0,0 +1,22 @@
+
+import { Increment, Decrement } from "../action/Count_action";
+
+const initialState = {
+    count: 0
+}
+
+const counterReducer = (state = initialState, action) => {
+    switch (action.type) {
+
+        case Increment:
+            return {
+                count: state.count + 1
+            };
+        case Decrement:
+            return {
+                count: state.count - 1
+            }
+        default:
+            return state;
+    }
+}
